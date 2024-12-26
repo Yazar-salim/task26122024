492d961f3ce572c4da3429240c4cb65a6e6b3671
yazar-salim <syazar222@gmail.com>
2024-12-26 22:56:05 +0530

    first commit for task dec26

---
+++ index.js
@@ -0,0 +1,79 @@
+// Write a  loop to iterate through the array and log only the numbers to the console, skipping any undefined values.
+
+// const arr = [10, undefined, 25, 'hello', 42, undefined, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] === 'number') {
+//     console.log(arr[i]);
+//   }
+// }
+
+
+//WAP to find the count of a given number in an array
+
+// const numbers = [1, 2, 3, 4, 2, 2, 5, 6];
+// const target = 2;
+// let count = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === target) {
+//       count++;
+//     }
+//   }console.log(count);
+  
+
+
+//Write a program to reverse an array
+// const originalArray = [1, 2, 3, 4, 5];
+// const reversedArray = [...originalArray].reverse(); 
+
+// console.log("Original Array:", originalArray);
+// console.log("Reversed Array:", reversedArray);
+
+//Remove neagative numbers in array
+// let number = [1, -2, 3, -4, 5, -6];
+// let result = [];
+
+// for (let i = 0; i < number.length; i++) {
+//   if (number[i] >= 0) {
+//     result.push(number[i]);
+//   }
+// }
+// console.log("Original Array:", number);
+// console.log("Array Without Negatives:", result);
+
+// //WAP to cancatenate 2 arrays
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let concatenatedArray = array1.concat(array2);
+// console.log("Concatenated Array:", concatenatedArray);
+
+
+
+//WAP to find the first even number in an array
+// let firstEven = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     firstEven = numbers[i];
+//     break; 
+//   }
+// }
+
+// if (firstEven !== null) {
+//   console.log("First Even Number:", firstEven);
+// } else {
+//   console.log("No even numbers found in the array.");
+// }
+
+
+//Write a JavaScript program to find all elements in an array that are greater than a given number n.
+
+// let numbers = [1, 7, 3, 10, 12, 5, 18];
+// let n = 6;
+// let result = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > n) {
+//     result.push(numbers[i]);
+//   }
+// }
+
+// console.log(`Elements greater than ${n}:`, result);
